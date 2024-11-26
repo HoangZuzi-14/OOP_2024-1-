09d3c540a12c0ef6682086003ce658a49319ff17
HoangZuzi-14 <146025761+HoangZuzi-14@users.noreply.github.com>
2024-11-26 13:26:01 +0700

    Delete lab3 directory

--- lab3/OtherProjects/src/hust/soict/dsai/lab1/61/src/61.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JOptionPane;
-public class bai7 {
-    public static void main(String[] args) {
-        int option = JOptionPane.showConfirmDialog(null, "Do you want to change to the first class ticket?");
-        JOptionPane.showMessageDialog(null, "You are chosen: " + (option == JOptionPane.YES_OPTION?"Yes":"No"));
-        System.exit(0);
-    }
-}
